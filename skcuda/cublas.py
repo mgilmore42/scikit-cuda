3d52da465e7309c68932a652d99af174a28bb8b6
Mitchell Gilmore <102109623+mgilmore42@users.noreply.github.com>
2023-07-17 11:39:13 -0400

    patched cublas

--- skcuda/cublas.py
+++ skcuda/cublas.py
@@ -26,8 +26,8 @@ from . import utils
 _linux_version_list = [11.0, 10.2, 10.1, 10.0, 9.2, 9.1, 9.0, 8.0, 7.5, 7.0, 6.5, 6.0, 5.5, 5.0, 4.0]
 _win32_version_list = [11, 10, 10, 100, 92, 91, 90, 80, 75, 70, 65, 60, 55, 50, 40]
 if 'linux' in sys.platform:
-    _libcublas_libname_list = ['libcublas.so'] + \
-                              ['libcublas.so.%s' % v for v in _linux_version_list]
+    _libcublas_libname_name = utils.resolve_lib('libcublas.so')
+    _libcublas_libname_list = [_libcublas_libname_name] if _libcublas_libname_name is not None else []
 elif sys.platform == 'darwin':
     _libcublas_libname_list = ['libcublas.dylib']
 elif sys.platform == 'win32':
