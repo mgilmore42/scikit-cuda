7e86e4a92f60321d10a9b739687af5bbc8202fcf
Mitchell Gilmore <102109623+mgilmore42@users.noreply.github.com>
2023-07-17 15:58:20 +0000

    patched cusparse

--- skcuda/cusparse.py
+++ skcuda/cusparse.py
@@ -21,8 +21,8 @@ import cuda
 _version_list = [11.0, 10.1, 10.0, 9.2, 9.1, 9.0, 8.0, 7.5, 7.0, 6.5, 6.0, 5.5, 5.0, 4.0]
 _win32_version_list = [110, 10, 10, 100, 92, 91, 90, 80, 75, 70, 65, 60, 55, 50, 40]
 if 'linux' in sys.platform:
-    _libcusparse_libname_list = ['libcusparse.so'] + \
-                                ['libcusparse.so.%s' % v for v in _linux_version_list]
+    _libcusparse_libname_name = utils.resolve_lib('libcusparse.so')
+    _libcusparse_libname_list = [_libcusparse_libname_name] if _libcusparse_libname_name is not None else []
 elif sys.platform == 'darwin':
     _libcusparse_libname_list = ['libcusparse.dylib']
 elif sys.platform == 'win32':
