e4fca774deab12013d2e61d80a18fdbfd4312728
Mitchell Gilmore <102109623+mgilmore42@users.noreply.github.com>
2023-07-17 15:50:00 +0000

    patched cufft

--- skcuda/cufft.py
+++ skcuda/cufft.py
@@ -10,13 +10,14 @@ import ctypes
 import operator
 import re
 import sys
+from . import utils
 
 # Load library:
 _linux_version_list = [11.0, 10.1, 10.0, 9.2, 9.1, 9.0, 8.0, 7.5, 7.0, 6.5, 6.0, 5.5, 5.0, 4.0]
 _win32_version_list = [110, 10, 10, 100, 92, 91, 90, 80, 75, 70, 65, 60, 55, 50, 40]
 if 'linux' in sys.platform:
-    _libcufft_libname_list = ['libcufft.so'] + \
-                             ['libcufft.so.%s' % v for v in _linux_version_list]
+    _libcufft_libname_name = utils.resolve_lib('libcufft.so')
+    _libcufft_libname_list = [_libcufft_libname_name] if _libcufft_libname_name is not None else []
 elif sys.platform == 'darwin':
     _libcufft_libname_list = ['libcufft.dylib']
 elif sys.platform == 'win32':
